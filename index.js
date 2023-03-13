1c291abe1e9a0d5871b7d3d41e2b73b772040f01
Garry <garryisrael02@gmail.com>
2023-03-13 03:21:59 -0400

    creates functions

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a/b;
+}
+
+function increment(n){
+  return ++n;
+}
+
+function decrement(n){
+  return --n;
+}
+
+function makeInt(n){
+  const numb = parseInt(n,10);
+  return numb;
+}
+
+function preserveDecimal (n){
+  const numb = parseFloat(n);
+  return numb;
+}
